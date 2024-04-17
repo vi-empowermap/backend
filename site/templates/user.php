9ed7b8523772061f990ea2b3ed7b3344ee16279e
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-17 15:25:44 +0200

    update

--- site/templates/user.php
+++ site/templates/user.php
@@ -1,21 +1,31 @@
-<?php snippet('header')?>
+<?php snippet('header') ?>
 <div class="user_container">
   <div class="user_wrapper">
     <div class="title"><?= $page->titlename() ?></div>
-  
+
     <div>
-      <div><?= $page->createuserbtn() ?></div>
       <form id="createForm" class="user_form">
-        <input id="userEmail" class="user_input user_text" type="email" name="email" placeholder="email" />
-        <input id="userPassword" class="user_input user_text" type="password" name="password" placeholder="password" />
-        <input id="userPasswordC" class="user_input user_text" type="password" name="password2" placeholder="password confirmation" />
-        <div id="passwordErrorM" class="error_message"></div>
-        <button class="user_input user_createuser_btn">Button</button>
+        <div>
+          <input id="userEmail" class="user_input user_text" type="email" name="email" placeholder="email" />
+        </div>
+        <div>
+          <input id="userPassword" class="user_input user_text" type="password" name="password" placeholder="password" />
+        </div>
+        <div>
+          <input id="userPasswordC" class="user_input user_text" type="password" name="password2" placeholder="password confirmation" />
+          <div id="passwordErrorM" class="error_message"></div>
+        </div>
+        <button class="user_input user_createuser_btn"><?= $page->createuserbtn() ?></button>
       </form>
-      <div class="userlogin_wrapper">
-        <a href="<?= $site->url() ?>/panel" class="user_input user_login">Log In</a>
-        <div class="user_input user_password">Forgot Password</div>
+      <div class="user_login">
+        <div><?= $page->loginbtn2() ?></div>
+        <a href="<?= $site->url() ?>/panel"><?= $page->loginbtn() ?></a>
       </div>
+      <div class="user_login">
+        <div><?= $page->forgetpasswordbtn2() ?></div>
+        <div class="user_password"><?= $page->forgetpasswordbtn() ?></div>
+      </div>
+
     </div>
   </div>
   <div class="password_wrapper">
@@ -24,7 +34,7 @@
       <form id="forgotPasswordForm" class="user_form">
         <input id="fUserEmail" class="user_input user_text" type="email" name="email" placeholder="email" />
         <div>Q1.blablabla</div>
-        <input id="pQuestion"  class="user_input user_text" type="text" name="question" placeholder="write..." />
+        <input id="pQuestion" class="user_input user_text" type="text" name="question" placeholder="write..." />
         <button class="user_input user_createuser_btn">Next</button>
       </form>
       <div class="userlogin_wrapper">
@@ -36,6 +46,6 @@
 </div>
 
 
-<?php snippet("user/user1")?>
+<?php snippet("user/user1") ?>
 
-<?php snippet('footer')?>
+<?php snippet('footer') ?>
