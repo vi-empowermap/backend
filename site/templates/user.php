b3be3d2510eb07bd2b388594407d2fd421d9603c
dain <57316771+DainPark-web@users.noreply.github.com>
2024-04-17 15:41:09 +0200

    update

--- site/templates/user.php
+++ site/templates/user.php
@@ -1,18 +1,20 @@
 <?php snippet('header') ?>
 <div class="user_container">
   <div class="user_wrapper">
+
     <div class="title"><?= $page->titlename() ?></div>
 
     <div>
       <form id="createForm" class="user_form">
         <div>
-          <input id="userEmail" class="user_input user_text" type="email" name="email" placeholder="email" />
+
+          <input id="userEmail" class="user_input user_text" type="email" name="email" placeholder="Email" />
         </div>
         <div>
-          <input id="userPassword" class="user_input user_text" type="password" name="password" placeholder="password" />
+          <input id="userPassword" class="user_input user_text" type="password" name="password" placeholder="Password" />
         </div>
         <div>
-          <input id="userPasswordC" class="user_input user_text" type="password" name="password2" placeholder="password confirmation" />
+          <input id="userPasswordC" class="user_input user_text" type="password" name="password2" placeholder="Confirm Password" />
           <div id="passwordErrorM" class="error_message"></div>
         </div>
         <button class="user_input user_createuser_btn"><?= $page->createuserbtn() ?></button>
@@ -32,14 +34,12 @@
     <div class="title">Forgot Password</div>
     <div>
       <form id="forgotPasswordForm" class="user_form">
-        <input id="fUserEmail" class="user_input user_text" type="email" name="email" placeholder="email" />
-        <div>Q1.blablabla</div>
-        <input id="pQuestion" class="user_input user_text" type="text" name="question" placeholder="write..." />
-        <button class="user_input user_createuser_btn">Next</button>
+        <input id="fUserEmail" class="user_input user_text" type="email" name="email" placeholder="Email" />
+        <input id="pQuestion" class="user_input user_text" type="text" name="question" placeholder="Answer" />
+        <button class="user_input user_createuser_btn"><?= $page->forgetpasswordbtn3() ?></button>
       </form>
-      <div class="userlogin_wrapper">
-        <a href="<?= $site->url() ?>/panel" class="user_input user_login">Log In</a>
-        <div id="createUserBtn" class="user_input user_btn">Create User</div>
+      <div class="user_login">
+        <div id="createUserBtn" class="user_btn">&larr; <?= $page->createuserbtn() ?></div>
       </div>
     </div>
   </div>
