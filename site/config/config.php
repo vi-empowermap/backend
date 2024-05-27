6989145679683334312b65c1c64668175d6e4cc4
dain <57316771+DainPark-web@users.noreply.github.com>
2024-05-27 10:22:17 +0200

    update

--- site/config/config.php
+++ site/config/config.php
@@ -1,13 +1,14 @@
 <?php
-
+// 2.Handling Preflight Requests in PHP
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
+    // header("Access-Control-Allow-Origin: http://localhost:3000");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     http_response_code(200);
     exit(0);
 }
-
+// 4.Setting Headers
 // Existing PHP logic
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, PUT");
